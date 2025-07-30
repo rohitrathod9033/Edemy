766fad6c97b7656f355281960c100d32014ddeb5
Rohit Rathod <rohitrathod60371@gmail.com>
2025-07-30 14:03:44 +0530

    clodinary complete

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,25 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/mongodb.js';
 import { clerkWebhooks } from './controllers/webhook.js';
+import educatorRouter from './routes/educatorRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
+import connectCloudinary from './configs/cloudinary.js'
 
 // Initailize Express
 const app = express();
 
 // Connect MongoDB
 await connectDB()
+await connectCloudinary()
 
 // Middlewares
 app.use(cors());
+app.use(clerkMiddleware())
 
+// Routes
 app.get('/', (req, res) => res.send("API Running"));
 app.post('/clerk', express.json(), clerkWebhooks);
+app.use('/api/educator', express.json(), educatorRouter);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
